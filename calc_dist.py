045d09fb1d3bcdfeded4d8d637bf836be8bb9963
jeff-lund <jdl3@pdx.edu>
2018-03-03 23:19:33 -0800

    working functions

--- calc_dist.py
+++ calc_dist.py
@@ -1,4 +1,4 @@
-import math
+from math import *
 
 # opening files
 f_cities = open('cities', 'r')
@@ -11,23 +11,27 @@ cities = [x.rstrip() for x in buf]
 f_cities.close()
 
 def haversines(la1, la2, lo1, lo2):
-	p = math.pi/180
-	a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
-	return 12742 * asin(sqrt(a)) #2*R*asin...
+	p = pi/180
+	a = 0.5 - cos((la2 - la1) * p)/2 + cos(la1 * p) * cos(la2 * p) * (1 - cos((lo2 - lo1) * p)) / 2
+	return 12742 * asin(sqrt(a))
 
 # writing long-lat list - long-lat
 buf = f_ll.readlines()
-long_lat = []
+f_ll.close()
+
+long_lat = {}
 for x in buf:
-	x = x.rstrip()
-	long_lat.append(x.strip(' '))
+	x = x.rstrip().split(' ')
+	long_lat[x[0]] = [x[1], x[2]]
 for x in long_lat:
-	x[1] = float(x[1])
-	x[2] = float(x[2])
+	long_lat[x][0] = float(long_lat[x][0])
+	long_lat[x][1] = float(long_lat[x][1])
+#print(long_lat.items())
 # Haversines
 for x in cities:
 	for y in cities:
 		if x == y:
 			continue
-		dist = haversines(x[1], y[1], x[2], y[2])
+		dist = haversines(long_lat[x][0], long_lat[y][0], long_lat[x][1], long_lat[y][1])
 		print(x, y, dist, file=fout)
+fout.close()
